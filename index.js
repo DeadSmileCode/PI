b092745e510042cc018c68d25545ba63a7e847af
DSC <sachev.andrei@gmail.com>
2023-03-01 14:54:45 +0600

    3th commit

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ const express = require("express");
 
 const app = express();
 
+console.log("start");
+
 app.use(express.static(__dirname + "/html"));
 
 app.get("/", function(req, res){
 	res.sendFile(__dirname + "/html/home.html");
-}
+});
 
 app.get("/home", function(req, res){
 	res.sendFile(__dirname + "/html/home.html");
-}
+});
 
-app.listen(80);
+app.listen(3000);
